3a8813f7a2a1b74f2469df2d1a49c21becdef12f
Andres Sewell <sewellandres@gmail.com>
2024-02-28 23:07:19 +0000

    Also install pyyaml

--- setup-polaris.sh
+++ setup-polaris.sh
@@ -49,7 +49,7 @@ EOF
 source $INSTALL_DIR/sourceme
 
 # Install Python dependencies
-pip install --upgrade pip setuptools wheel numpy mpi4py jupyterlab notebook
+pip install --upgrade pip setuptools wheel numpy mpi4py jupyterlab notebook pyyaml
 
 # Modify Ascent build script
 cd $INSTALL_DIR/ascent/scripts/build_ascent/
